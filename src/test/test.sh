4cabf27e916bcaf6dcc396f72fb8348b72476bf7
Porter Liu <liuzhf@gmail.com>
2013-01-07 12:49:32 +0800

    add test files

---
+++ src/test/test.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python ../picwall.py s=512x384 p=20 c=3 b=50,50,50 t=200,200,200 f=TempoFontItalic.ttf o=out.png 01.jpg "Monday" 02.jpg "Tuesday" 03.jpg "Wednesday" 04.jpg "Thursday" 05.jpg "Friday" 06.jpg "Saturday"
